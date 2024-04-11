953e2e90dc7fdf486c30daa59711087865444c37
unknown <hajikhanih@code.edu.az>
2024-04-11 23:40:27 +0400

    fixed User repository And IGroupRepo

--- Repository/Repositories/GroupRepository.cs
+++ Repository/Repositories/GroupRepository.cs
@@ -5,6 +5,24 @@ namespace Repository.Repositories
 {
     public class GroupRepository : BaseRepository<Group>, IGroupRepository
     {
-      
+        public Task<List<Group>> FilterByEducationNameAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Group>> GetAllWithEducationIdAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Group>> SearchAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Group>> SortWithCapacityAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
